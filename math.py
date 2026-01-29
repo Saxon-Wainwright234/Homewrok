5122e2ba71fd1e5fed2764d73529dc418e48c0b4
Saxon-Wainwright234 <saxonwainwright06@gmail.com>
2026-01-29 23:06:51 +0000

    mmst

---
+++ math.py
@@ -0,0 +1,21 @@
+import math 
+
+#truncated 
+
+def truncate(number):
+    factor = 10 ** 7
+    return math.trunc(number * factor) / factor
+
+#inputs 
+
+V1 = float(input("V1: "))
+V2 = float(input("V2: "))
+angle = math.radians(float(input("Angle: ")))
+
+#math 
+
+A = V1 * math.cos(angle) + V2 * math.sin(angle)
+B = (-V1) * math.sin(angle) + V2 * math.cos(angle)
+
+print(truncate(A))
+print(truncate(B))
